1e48b14d8654134cca7193a401163f87f9247101
Marc <marc.ferraro1@gmail.com>
2021-01-10 19:50:17 -0600

    commit tree 69995a194064b0632012c22af859f77c59f84b69

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    resp = Rack::Response.new
+
+    get '/name' do
+        resp.status = 200
+        "My name is Marc"
+    end
+
+    get '/hometown' do
+        resp.status = 200
+        "My hometown is Chicago"
+    end
+
+    get '/favorite-song' do
+        resp.status = 200
+        "My favorite song is impossible to decide"
+    end
 end
